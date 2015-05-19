eb151dc7ef1f19db480defd5e84317a3a80dbc8b
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2015-05-19 09:38:27 +0200

    processing jpsi samples

--- test/createAndRunDataCrab.py
+++ test/createAndRunDataCrab.py
@@ -16,14 +16,14 @@ d = datetime.datetime.now().strftime("%y%b%d")
 version = 2
     
 datasets = [
-    ["/MuHad/ebouvier-MuHad_Run2012A-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "MuHad_Run2012A", "10"],
-    ["/ElectronHad/ebouvier-ElectronHad_Run2012A-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "ElectronHad_Run2012A","14"],
-    ["/SingleMu/ebouvier-SingleMu_Run2012B-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleMu_Run2012B", "2"],
-    ["/SingleElectron/ebouvier-SingleElectron_Run2012B-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleElectron_Run2012B", "3"],
-    ["/SingleMu/ebouvier-SingleMu_Run2012C-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleMu_Run2012C", "2"],
-    ["/SingleElectron/ebouvier-SingleElectron_Run2012C-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleElectron_Run2012C", "3"],
-    ["/SingleMu/ebouvier-SingleMu_Run2012D-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER","SingleMu_Run2012D", "2"],
-    ["/SingleElectron/ebouvier-SingleElectron_Run2012D-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER","SingleElectron_Run2012D", "3"]
+    ["/MuHad/ebouvier-MuHad_Run2012A-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "MuHad_Run2012A", "100"],
+    ["/ElectronHad/ebouvier-ElectronHad_Run2012A-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "ElectronHad_Run2012A","140"],
+    ["/SingleMu/ebouvier-SingleMu_Run2012B-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleMu_Run2012B", "20"],
+    ["/SingleElectron/ebouvier-SingleElectron_Run2012B-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleElectron_Run2012B", "30"],
+    ["/SingleMu/ebouvier-SingleMu_Run2012C-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleMu_Run2012C", "20"],
+    ["/SingleElectron/ebouvier-SingleElectron_Run2012C-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER", "SingleElectron_Run2012C", "30"],
+    ["/SingleMu/ebouvier-SingleMu_Run2012D-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER","SingleMu_Run2012D", "20"],
+    ["/SingleElectron/ebouvier-SingleElectron_Run2012D-22Jan2013_15Apr01-v1-0a0811ab37aa24280a978161285f4b48/USER","SingleElectron_Run2012D", "30"]
     ]
 
 print("Creating configs for crab. Today is %s, you are %s and it's version %d" % (d, user_name, version))
